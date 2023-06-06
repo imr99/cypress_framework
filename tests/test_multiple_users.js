4081c9a54bb541419e559464cb4a2fe998cd838f
Bishwaroop Chakraborty <Bishwaroop@Brickredsys.in>
2023-06-06 18:11:43 +0530

    Mutliple users

--- tests/test_multiple_users.js
+++ tests/test_multiple_users.js
@@ -12,6 +12,11 @@ const flash = require('express-flash')
     const passport = require('passport');
 const { cleansCode } = require('../utils/cleanData.js');
 
+var cookieSession = require('cookie-session')
+var express = require('express')
+var app = express()
+
+
 var url = null;
 var uri = null;
 var environementUrl = null;
@@ -28,6 +33,24 @@ describe('BSS Sanity Suite', () => {
 
     it('Create User - CRM Account List', async function () {
         try {
+
+            app.set('trust proxy', 1) 
+            /**
+             * var secrets = []
+             */
+            app.use(cookieSession({
+                name: 'session',
+                keys: ['a5UFK8Z6gQOQTihV1eZLSUNVMeHZrluYLJAAAABm', '64MsW4nco1gYD43rN7vsDvuvACUTn-5kvfcAAABm'],
+              
+                // Cookie Options
+                maxAge: 24 * 60 * 60 * 1000 // 24 hours
+              }))
+
+              app.use(function (req, next) {
+                req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
+                next()
+              })
+
             url = apiConfigJson.preprod.url;
             uri = apiConfigJson.preprod.USER_LOGIN_URI;
             environementUrl = url + "/" + uri;
@@ -66,8 +89,11 @@ describe('BSS Sanity Suite', () => {
             // const cookieData = headersConfig + postmanToken + pfData
             // console.log('MY COOKIE DATA - '+cookieData);
             const cookieData = 'avs_cookie=' + headersConfig.avs_cookie + ';up=' + headersConfig.up + ';telus_user_profile=' + headersConfig.telus_user_profile 
-            +';sessionId=' + headersConfig.sessionId
-            + ';telus_refresh_cookie=' +  headersConfig.telus_refresh_cookie + ';telus_access_cookie=' + headersConfig.telus_access_cookie
+              +';sessionId=' + headersConfig.sessionId
+              + ';telus_refresh_cookie=' +  headersConfig.telus_refresh_cookie + ';telus_access_cookie=' + headersConfig.telus_access_cookie
+
+           // const cookieData = 'avs_cookie=eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiU3drVmNNeFFSWlUrV3lPSlozRzBVd01WSEM3MzlWdXZ5eXdJRUtzL00rNDZPUGRKYmQ0QUZpWnVLWVBaZUpqUk5oT3lyQm5BNGcwMlVCMkE4UExKdjBITVBjNTd5Z0JXVXZ2NklTY1o2eHNNdWppOUg4UWhMbWYvWGs5aGdXcVJvNWZGSXl0SzJEQU1LcERJbUM3SVNpUXhiT0tpUDhOOHZPUEFWYytJZ08zMW5JK2FEYm94cjBUZlFmbTFMcEUvayt4NUZETDN5ajNUSHMwbCtwRUM3cVpZZE9lSjZGUkk1OFprREY5KzE1VHR3b0JpTUUxemdwVTRtcks4bGpYU2szZGN3cDFkZFc5em9zL1pGdTBQclU4eHh5ZFVPSVE3NHBRVldaQ1RiQkNhVzNMUFNSdHFoQldkSjFuRVhsaVYzVjFPYWswbjY0eUgzZW9aS29wcFlVQUJFY2s0RmN6YVYxRlFod1hFNWRaTFE4NFBlUVYzOWRkZ1AxdW0xWHNydXdUbVdmejFieGNsYkxDTzJrTkZ0VFBNcWorZllvbGRadkkvMy9vMEpsZmhGNWZhbHgrSWNROXB4MkRhOG80ZlR5MGt4Zz09Iiwibm9uY2UiOiIvalVZOERzMVdjblczUEp5IiwiaXNzIjoiQVZTIiwiZXhwIjoxNjg1OTgxOTgzfQ.CKDxfcin7TUD1r_qgFBEgJAyzZiV2B8ZP9-uHjk04WY;up=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjg5MCwidXNlcm5hbWUiOiJvcHVzLnNxZDA0LnRzMDE2IiwidXNlclBDTGV2ZWxFcGciOjYwLCJ1c2VyUGNFeHRlbmRlZFJhdGluZ3MiOiIiLCJ1c2VyUGFyZW50YWxDb250cm9sIjoiTiIsInByb3BlcnR5IjpbeyJwcm9wZXJ0eU5hbWUiOiJPUFRJSyJ9XSwiaXNPdXRPZkhvbWUiOiJOIiwiaWF0IjoxNjg1OTY3NTgzLCJleHAiOjE2OTM3NDM1ODN9.o9uZag-AgFb03bZSpqfwKU7jY6skV5FQzvoSGguNdys;telus_user_profile=df7d3ef66f5f96453ee4a52a486dcf8a4d000f44fc1bf30102c8fad173507b33e9f1e9bbe3f1e7d8da0a5feceab4fda122bacb9642ffdd05c921be1a14ec9b256ff7d74b30ae9214865fceb1418e0845930b337fe9a98e6c0091510fe479ac0f172dd86152566f6d6d117958561bedeb22810ebd674900c826a2f70e26ad3db0;sessionId=7dc74fa2-2537-f10c-ebfd-811763e91654;telus_refresh_cookie=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWx1c19yZWZyZXNoX2Nvb2tpZSI6ImJhYWIwNjhlNWI5NjNhMGU4MWE5NDdkNTIyZjJjMDllZTQxM2Q1NjZjOTFlM2QyYWFlMWY0MTFlOTU3NjViYmY5MjNmNDgzOThjNzBhZjc2NTk5MTVkYzk2MWYyNDRkMiIsImV4cCI6MTcxNzUwMzU4MywiaXNzIjoiQVZTIn0.iaXVfUbURwa7GFoeAGfh2V5QlGylgDK-shoO4hY64Ts;telus_access_cookie=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWx1c19hY2Nlc3NfY29va2llIjoiYzliNzI2NjhlZWY0YWYwZGY1MGM2MWZmMzI0NzQ4NzI2ZDhlNTBhOWRhOWRlM2ZlZjBiNWJmMDlhZThmMzY0MSIsImV4cCI6MTY4NTk4MTk4MiwiaXNzIjoiQVZTIn0.NuPN-oHrvdqZHpagWbm3-LTapoVuXCWLKukAt04WRiQ';
+
             console.log("cookieData : - ", cookieData);
             console.log('ENV URL ', environementUrl)
 
